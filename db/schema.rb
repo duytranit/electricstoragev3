f030c6c257eb414e412e5e083b39f32f2c8fcdc6
duytranit <duytran.it@gmail.com>
2014-09-12 09:49:56 +0700

    Re-generate scaffold project with gem haml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912024041) do
+ActiveRecord::Schema.define(version: 20140912024835) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
