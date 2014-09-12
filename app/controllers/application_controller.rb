dbc05224d15d46c26b86a5ba0bf49ca6ba1b2808
duytranit <duytran.it@gmail.com>
2014-09-12 10:22:12 +0700

    I18n
    Intial I18n vi and en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
+
+  private
+  def set_locale
+    if cookies[:locale].nil?
+      cookies[:locale] = 'vi'
+    end
+    if params[:locale].present?
+      cookies[:locale] = params[:locale]
+    end
+    I18n.locale = cookies[:locale]
+  end
 end
