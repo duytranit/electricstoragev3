56a11c1f934c8dc17ccc35972531cbee0cc933d7
duytranit <duytran.it@gmail.com>
2014-10-01 16:36:09 +0700

    active admin project

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Project do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :user, :procategory, :title, :description, :status, :photo
   #
   # or
   #
@@ -21,6 +21,33 @@ ActiveAdmin.register Project do
 			p.procategory.name
 		end
 		actions
+	end
+
+	show do |p|
+		attributes_table do
+			row :photo do
+				image_tag(p.photo.url(:medium))
+			end
+			row :title
+			row :user
+			row :procategory
+			row :description
+			row :created_at
+			row :updated_at
+			row :status
+		end
+	end
+
+	form do |f|
+		f.inputs do
+			f.input :procategory
+			f.input :user, as: :select, :collection => staffs.map{|u| [u.email, u.id]}
+			f.input :title
+			f.input :description
+			f.input :photo, as: :file
+			f.input :status
+		end
+		f.actions
 	end
 
 	filter :procategory, as: :select, :collection => ordered_procategories.map{|c| [c.full_name.truncate(50), c.id]}
