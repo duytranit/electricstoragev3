a9dd83778810ddf571ea4de414a745fd5605dc29
duytranit <duytran.it@gmail.com>
2014-10-01 16:55:26 +0700

    activeadmin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,13 +3,20 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
+    table_for Project.all.limit(5) do
+	    column :title do |p|
+		    link_to p.title, [:admin, p]
+	    end
+	    column :procategory
+    end
+    strong { link_to "View all projects", admin_projects_path }
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
